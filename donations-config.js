d01accd7cc8ee133897541688ccd4ec4efd8a3ab
AdminAstrum01 <admin@redastrum.org>
2026-08-21 11:48:28 -0500

    Update API base URL and Mercado Pago public key

--- donations-config.js
+++ donations-config.js
@@ -5,8 +5,8 @@
  */
 window.RED_ASTRUM_DONATIONS = {
     environment: "test",
-    apiBaseUrl: "", // e.g. https://donations-api.redastrum.org
-    mercadoPagoPublicKey: "", // TEST public key only while developing
+    apiBaseUrl: "", // e.g. https://red-astrum-donations.curly-grass-94b6.workers.dev
+    mercadoPagoPublicKey: "", // APP_USR-1809599944828758-082111-5687cf01d20fba7d40277052db832f68-3632101616
     allowedAmounts: [10, 25, 50, 100],
     cci: {
         bank: "",
