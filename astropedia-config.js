e102af8bc3ebe87b15570a81cd1e3afcda1a2077
AdminAstrum01 <admin@redastrum.org>
2026-08-31 16:37:31 -0500

    Corregir autenticación 401 de Astropedia
    * Corregir la clave pública activa de Astropedia
    
    * Distinguir errores de configuración de Supabase
    
    * Actualizar caché de Astropedia a 3.0.1
    
    * Sincronizar la ruta de Astropedia 3.0.1

--- astropedia-config.js
+++ astropedia-config.js
@@ -1,4 +1,4 @@
 window.ASTROPEDIA_CONFIG = Object.freeze({
     url: "https://sefuivmghelmajxccnmv.supabase.co",
-    publishableKey: "sb_publishable_ltaNA7nnVozoSCOcZIjg"
+    publishableKey: "sb_publishable_SCQqPT2Xvx8-h3FPub45Bg_beWRHINR"
 });
